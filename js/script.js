f951be3ce70a59fe1c706e8f3014f2534db106c5
Paul LC <plecoz.pro@gmail.com>
2024-10-07 13:06:24 +0200

    added rain

---
+++ js/script.js
@@ -0,0 +1,22 @@
+function createRaindrop() {
+    const rainContainer = document.querySelector('.rain-container');
+
+    const raindrop = document.createElement('div');
+    raindrop.classList.add('raindrop');
+
+    raindrop.style.left = `${Math.random() * 100}vw`; 
+    raindrop.style.animationDuration = `${Math.random() * 2 + 0.5}s`;  
+    raindrop.style.opacity = Math.random(); 
+
+    rainContainer.appendChild(raindrop);
+
+    setTimeout(() => {
+        rainContainer.removeChild(raindrop);
+    }, 3000); 
+}
+
+function makeItRain() {
+    setInterval(createRaindrop, 50);  // Create a new raindrop every 50ms
+}
+
+makeItRain();
